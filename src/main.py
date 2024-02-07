bca75b588eca3e12c5f3aaab3a49703f9ede52fd
orafamoura <raphaelmvb2@gmail.com>
2024-02-07 19:37:54 -0300

    config: Config project settings

---
+++ src/main.py
@@ -0,0 +1,16 @@
+def soma(
+    num_1: int, num_2: int
+) -> (
+    int
+):  # colocamos o : int e -> int para ajudar o proximo desenvolvedor entender o que aceita
+    """Adding two numbers
+    :param num_1: number 1
+    :param num_2: number 2
+    :return: sum of the two numbers
+    """
+
+    sum_two_numbers = num_1 + num_2
+    return sum_two_numbers
+
+
+# SUM_TWO_NUMBERS = soma(1, 2)
